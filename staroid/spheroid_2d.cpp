262985756e008c75a92533fca3239c3d313f5568
janderkkotlarski <j.d.kotlarski@gmx.com>
2021-04-07 18:17:19 +0200

    Looks to be simpler than expected

--- staroid/spheroid_2d.cpp
+++ staroid/spheroid_2d.cpp
@@ -4,3 +4,8 @@ spheroid_2d::spheroid_2d()
 {
 
 }
+
+void spheroid_2d::display()
+{
+
+}
--- staroid/spheroid_2d.h
+++ staroid/spheroid_2d.h
@@ -1,11 +1,24 @@
 #ifndef SPHEROID_2D_H
 #define SPHEROID_2D_H
 
+#include "raylib.h"
 
 class spheroid_2d
 {
+private:
+  const Vector3 m_pos
+  { 0.0f, 0.0f, 0.0f };
+
+  const Vector3 m_dir
+  { 1.0f, 0.0f, 0.0f };
+
+  const float m_radius
+  { 1.0f };
+
 public:
   spheroid_2d();
+
+  void display();
 };
 
 #endif // SPHEROID_2D_H
